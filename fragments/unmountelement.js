184b02a77f684d4c34aca22f23556ddd39a596a3
davidpitman <davidpitman@penguin>
2025-01-25 18:56:20 +1000

    working on it...

--- fragments/unmountelement.js
+++ fragments/unmountelement.js
@@ -1,6 +1,7 @@
 export default async function (element) {
     if (!(element instanceof HTMLElement)) return
     // if (this.isFacetContainer(element)) return await this.unmountFacet(element)
+    // change this to an optional data-bind attribute on anchors combined with a WeakMap between the anchor element and the unit instance in UI.app
     if (element.children.length) {
         const promises = []
         for (const n of element.children) promises.push(this.unmountElement(n))
