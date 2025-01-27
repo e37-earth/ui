6028df29b50d6a8cba632b267e93a2fd888a0ede
davidpitman <davidpitman@penguin>
2025-01-27 17:30:05 +1000

    working on it...

--- fragments/anchorconditionals.js
+++ fragments/anchorconditionals.js
@@ -55,20 +55,22 @@ const doComparison = (currentValue, compareWith) => {
     }
 
 export default {
-    E37: async (anchorElement, subConditions, compareWith, unit, toggleAble, once) => {
+    E37: async (anchorElement, subConditions, compareWith, toggleAble, unit, once) => {
         const scopeValue = { UI: this }
         let currentValue = scopeValue
         for (const condition of subConditions) currentValue = currentValue?.[condition.trim()]
-        if (!unit) return doComparison(currentValue, compareWith)
-        const whenWatcher = { active: undefined, target: new EventTarget(), callback: enable => unit.toggle(enable), once }
-        whenWatcher.callback = requestIdleCallback(() => {
-            let currentValue = scopeValue
-            for (const condition of subConditions) currentValue = currentValue?.[condition.trim()]
-            whenWatcher.active = doComparison(currentValue, compareWith)
-            whenWatcher.target.dispatchEvent(new CustomEvent('change', { detail: whenWatcher.active }))
-        })
-        this.app._anchorWhenWatchers.set(anchorElement, whenWatcher)
-        return whenWatcher
+        const currentlyEnabled = doComparison(currentValue, compareWith)
+        if (toggleAble) {
+            const whenWatcher = { active: undefined, target: new EventTarget(), callback: enable => unit.toggle(enable), once }
+            whenWatcher.callback = requestIdleCallback(() => {
+                let currentValue = scopeValue
+                for (const condition of subConditions) currentValue = currentValue?.[condition.trim()]
+                whenWatcher.active = doComparison(currentValue, compareWith)
+                whenWatcher.target.dispatchEvent(new CustomEvent('change', { detail: whenWatcher.active }))
+            })
+            this.app._anchorWhenWatchers.set(anchorElement, whenWatcher)
+        }
+        return currentlyEnabled
     },
     dev: async (anchorElement, subConditions, compareWith, whenCallback, once) => !!this.modules.dev === normalizeCompareWith(compareWith || true),
     location: async (anchorElement, subConditions, compareWith, whenCallback, once) => {
